f134618d2d5056ef1169e8679ea0a4f125b581f6
ellaj5 <164180194+ellaj5@users.noreply.github.com>
2024-11-01 14:14:39 +0900

    Add mento-application page

---
+++ src/component/mento-application/MentoApplication.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Menti from './Menti';
+
+function MentiManage() {
+  return (
+    <div className = "menti-manage">
+        <Menti />
+    </div>
+  );
+}
+
+export default MentiManage;
---
+++ src/component/mento-application/Register.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Register.css';
+import Profile from '../main-page/Profile'
+
+function MentoApplication () {
+    return (
+        <div className = "mento-application">
+            <div className = "mento-application-title">멘토 신청</div>
+            <div className = "mento-application-university">대학 정보</div>
+            <div className = "mento-application-major">학과</div>
+            <div className = "mento-application-grade">학년</div>
+            <div className = "mento-application-enrollment">재학 상태</div>
+            <div className = "mento-application-submit">재학증명서/졸업증명서 제출</div>
+            <div className = "mento-application-file-upload"></div>
+        </div>
+    );
+}
+
+export default Mentimanage;
